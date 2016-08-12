5f268fd8cb071e94c142dc4b9a78c0efcfea7493
dmt <dmt.malin@gmail.com>
2016-08-12 13:30:03 +0300

    tasks-database Rubric in cache

--- digest/models.py
+++ digest/models.py
@@ -2,12 +2,31 @@
 from __future__ import unicode_literals
 
 import architect
+from django.core.cache import cache
 
 from django.db import models
 
 
+class RubricQuerySet(models.QuerySet):
+    def cache_get_or_create(self, key, defaults=None, **kwargs):
+
+        try:
+            cache_key = kwargs[key]
+        except KeyError:
+            raise AttributeError(u'Cache key not contained in transmitted model params')
+
+        rubric, created = cache.get(cache_key), False
+        if not rubric:
+            rubric, created = super(RubricQuerySet, self).get_or_create(defaults, **kwargs)
+            cache.set(cache_key, rubric)
+
+        return rubric, created
+
+
 class Rubric(models.Model):
     title = models.CharField(u'Название', max_length=255)
+
+    objects = RubricQuerySet.as_manager()
 
     class Meta:
         verbose_name = u'Рубрика'
